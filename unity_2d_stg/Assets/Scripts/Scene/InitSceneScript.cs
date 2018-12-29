a625c6811592746e8fe6ea9a29b77f2b024427f5
Kur0qb <gdgdkuro@gmail.com>
2018-12-29 19:43:42 +0900

    MainMenuSceneを追加、TitleSceneからMainMenuSceneへつなげるようにした

--- unity_2d_stg/Assets/Scripts/Scene/InitSceneScript.cs
+++ unity_2d_stg/Assets/Scripts/Scene/InitSceneScript.cs
@@ -7,8 +7,8 @@ public class InitSceneScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
---
+++ unity_2d_stg/Assets/Scripts/Scene/TitleScene/TitleSceneTask.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleSceneTask : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SceneShare.Instance.Initialize();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        UnitySingleTouchAction touchAction = SceneShare.Instance.GetInput().GetSingleTouchAction();
+        touchAction.Update();
+        if (touchAction.IsTouchBegan()) {
+            MhCommon.Print("TitleScene -> MainMenuScene");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenuScene");
+        }
+        //UnityEngine.SceneManagement.SceneManager.LoadScene("DebugMenuScene");
+    }
+}
