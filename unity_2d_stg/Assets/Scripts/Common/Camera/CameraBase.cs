0d1feb9e9bf447ef2b1a9de96091bbf3a3f989fc
Kur0qb <gdgdkuro@gmail.com>
2018-12-29 18:43:17 +0900

    TitleSceneの追加

---
+++ unity_2d_stg/Assets/Scripts/Common/Camera/CameraBase.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBase : MonoBehaviour {
+
+    public CameraBase() {
+    }
+
+    private void Awake() {
+        // アスペクト比の固定
+        Camera camera = GetComponent<Camera>();
+        Rect rect = CalcAspect(aspectX, aspectY);
+        camera.rect = rect;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// アスペクト比の計算
+    /// </summary>
+    /// <param name="width"></param>
+    /// <param name="height"></param>
+    /// <returns></returns>
+    Rect CalcAspect(float width, float height) {
+        float targetAspect = width / height;
+        float windwAspect = (float)Screen.width / (float)Screen.height;
+        float scaleHeight = windwAspect / targetAspect;
+        Rect rect = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
+
+        if (scaleHeight < 1.0f) {
+            rect.x = 0;
+            rect.y = (1.0f - scaleHeight) / 2.0f;
+            rect.width = 1.0f;
+            rect.height = scaleHeight;
+        } else {
+            float scaleWidth = 1.0f / scaleHeight;
+            rect.x = (1.0f - scaleWidth) / 2.0f;
+            rect.y = 0.0f;
+            rect.width = scaleWidth;
+            rect.height = 1.0f;
+        }
+        return rect;
+    }
+
+    public float aspectX = 9.0f;
+    public float aspectY = 16.0f;
+}
---
+++ unity_2d_stg/Assets/Scripts/Common/System/Scene/SceneBase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract class SceneBase : ISceneTask {
+
+    /// <summary>
+    /// シーン初期化処理
+    /// </summary>
+    void ISceneTask.Initialize() {
+    }
+
+    /// <summary>
+    /// シーン開放処理
+    /// </summary>
+    void ISceneTask.Release() {
+    }
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    void ISceneTask.Update() {
+    }
+
+    /// <summary>
+    /// スマートフォン用非アクティブイベント
+    /// </summary>
+    void ISceneTask.Suspend() {
+    }
+
+    /// <summary>
+    /// スマートフォン用アクティブイベント
+    /// </summary>
+    void ISceneTask.Resume() {
+    }
+}
