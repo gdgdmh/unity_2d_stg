d6b5c46a4ea96ea0f28387661cdb4f489a496656
Kur0qb <gdgdkuro@gmail.com>
2019-01-28 16:07:20 +0900

    プレイヤーの移動処理を追加

---
+++ unity_2d_stg/Assets/Scripts/Scene/GameMainScene/GameMainSceneTask.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMainSceneTask : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        SceneShare.Instance.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
