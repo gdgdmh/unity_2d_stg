8c3ebad43a355132895d20e1228d261e62a6bf50
Kur0qb <gdgdkuro@gmail.com>
2019-02-03 19:48:58 +0900

    Enemy1の回転を仮実装

---
+++ unity_2d_stg/Assets/Scripts/Stg/Game/Enemy/EnemyBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : StgGameObject {
+
+	public EnemyBase() {
+	}
+
+	public override void Initialize() {
+		base.Initialize();
+	}
+
+    // Update is called once per frame
+    public override void Update() {
+    }
+}
---
+++ unity_2d_stg/Assets/Scripts/Stg/Game/Enemy/NormalEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalEnemy : EnemyBase
+{
+	public NormalEnemy() {
+	}
+
+	public override void Initialize() {
+	}
+
+	// Start is called before the first frame update
+	public override void Start() {
+    }
+
+    // Update is called once per frame
+    public override void Update() {
+		//Transform transform = GetComponent<Transform>();
+		//MhCommon.Assert(transform != null, "NormalEnemy::Update transform null");
+		Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>();
+		MhCommon.Assert(rigidbody2D != null, "NormalEnemy::Update transform null");
+
+		rigidbody2D.angularVelocity = 30.0f;
+
+    }
+}
