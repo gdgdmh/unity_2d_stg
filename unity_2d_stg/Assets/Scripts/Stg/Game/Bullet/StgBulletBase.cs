c5b73fe609710d86156b198f6ed79bcc1a96bfc0
Kur0qb <gdgdkuro@gmail.com>
2019-02-10 13:14:20 +0900

    StgBulletBaseにStgGameObjectを継承させるように変更

--- unity_2d_stg/Assets/Scripts/Stg/Game/Bullet/StgBulletBase.cs
+++ unity_2d_stg/Assets/Scripts/Stg/Game/Bullet/StgBulletBase.cs
@@ -2,23 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StgBulletBase : MonoBehaviour, IStgAttack {
+public class StgBulletBase : StgGameObject, IStgAttack {
 
 	public StgBulletBase() {
 	}
 
     // Start is called before the first frame update
-    public void Start() {
+    public override void Start() {
     }
 
     // Update is called once per frame
-    public void Update() {
+    public override void Update() {
     }
 
 	/// <summary>
 	/// 攻撃(IStgAttackのオーバーライド)
 	/// </summary>
 	/// <param name="stgGameObject"></param>
-	public void Attack(StgGameObject stgGameObject) {
+	public virtual void Attack(StgGameObject stgGameObject) {
 	}
 }
