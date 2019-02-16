2086868c97d52cf6d5819e7c3c0d0feb45ce7b6a
Kur0qb <gdgdkuro@gmail.com>
2019-02-16 16:35:52 +0900

    EnemyのPrefabの位置を変更(Resourceから参照が可能なように) StgEnemyFactoryを追加 ゲーム開始時に動的に敵を追加するようにした

--- unity_2d_stg/Assets/Scripts/Scene/GameMainScene/GameMainSceneTask.cs
+++ unity_2d_stg/Assets/Scripts/Scene/GameMainScene/GameMainSceneTask.cs
@@ -7,6 +7,16 @@ public class GameMainSceneTask : MonoBehaviour
     // Start is called before the first frame update
     void Start() {
         SceneShare.Instance.Initialize();
+
+		{
+			// 敵の生成
+			GameObject player = GameObject.Find("Player");
+			MhCommon.Assert(player != null, "GameMainSceneTask::Start() player null");
+			StgEnemyFactory factory = new StgEnemyFactory(player);
+			GameObject enemy = factory.Create(StgEnemyConstant.Type.kEnemyNormal);
+			Instantiate(enemy, new Vector3(0.0f, 4.0f, 0.0f), Quaternion.identity);
+		}
+
 		/*
 		// PlayerBulletを動的生成
 		{
---
+++ unity_2d_stg/Assets/Scripts/Stg/Game/Enemy/StgEnemyFactory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StgEnemyFactory : AStgEnemyFactory {
+
+	public StgEnemyFactory(GameObject player) {
+		this.player = player;
+	}
+
+	public override GameObject Create(StgEnemyConstant.Type type) {
+		if (type == StgEnemyConstant.Type.kEnemyNormal) {
+			GameObject enemy = (GameObject)Resources.Load("Prefabs/Game/Enemy/Enemy1");
+			MhCommon.Assert(enemy != null, "StgEnemyFactory::Create() kEnemyNormal null");
+			return enemy;
+		} else {
+			MhCommon.Assert(false, "StgEnemyFactory::Create() StgEnemyConstant.Type invalid type=" + type);
+			return null;
+		}
+	}
+
+	protected GameObject player;
+}
