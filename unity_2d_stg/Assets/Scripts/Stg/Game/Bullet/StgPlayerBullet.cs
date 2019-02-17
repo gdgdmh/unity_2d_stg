0c0268957faaf58231cb1f72873de19086f27e6f
Kur0qb <gdgdkuro@gmail.com>
2019-02-17 16:51:04 +0900

    プレイヤーの弾のエリアオーバー処理を追加

--- unity_2d_stg/Assets/Scripts/Stg/Game/Bullet/StgPlayerBullet.cs
+++ unity_2d_stg/Assets/Scripts/Stg/Game/Bullet/StgPlayerBullet.cs
@@ -17,7 +17,36 @@ public class StgPlayerBullet : StgBulletBase {
 		MhCommon.Assert(rigidbody2D != null, "NormalEnemy::Update transform null");
 		// 移動
 		rigidbody2D.velocity = new Vector2(0.0f, 15.0f);
+
+		// エリアオーバー処理
+		ProcessAreaOver(rigidbody2D);
     }
+
+	/// <summary>
+	/// エリアオーバー処理
+	/// </summary>
+	/// <param name="rigidbody2D"></param>
+	protected void ProcessAreaOver(Rigidbody2D rigidbody2D) {
+		if (IsAreaOver(rigidbody2D)) {
+			Destroy(this.gameObject);
+		}
+	}
+
+	/// <summary>
+	/// エリアオーバーしているか
+	/// </summary>
+	/// <param name="rigidbody2D"></param>
+	/// <returns></returns>
+	protected bool IsAreaOver(Rigidbody2D rigidbody2D) {
+		float x = rigidbody2D.position.x;
+		float y = rigidbody2D.position.y;
+
+		if ((x > 10.0f) || (x < -10.0f) || (y > 10.0f) || (y < -10.0f)) {
+			return true;
+			//Destroy(this.gameObject);
+		}
+		return false;
+	}
 
 	/// <summary>
 	/// 攻撃(IStgAttackのオーバーライド)
