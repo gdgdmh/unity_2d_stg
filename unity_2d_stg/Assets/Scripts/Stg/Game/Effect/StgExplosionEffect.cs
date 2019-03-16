ffb376f4406278627f2b315898dedb4f93779f00
Kur0qb <gdgdkuro@gmail.com>
2019-03-16 17:29:06 +0900

    クラスを説明を追加

--- unity_2d_stg/Assets/Scripts/Stg/Game/Effect/StgExplosionEffect.cs
+++ unity_2d_stg/Assets/Scripts/Stg/Game/Effect/StgExplosionEffect.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 爆発用エフェクトスクリプト
+/// </summary>
 public class StgExplosionEffect : MonoBehaviour {
+	// エフェクト終了時イベント
 	public void OnAnimationFinish() {
 		// アニメーションが終了したら消滅
 		Destroy(gameObject);
--- unity_2d_stg/Assets/Scripts/Stg/Game/Enemy/AStgEnemyFactory.cs
+++ unity_2d_stg/Assets/Scripts/Stg/Game/Enemy/AStgEnemyFactory.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 敵生成クラス
+/// </summary>
 public abstract class AStgEnemyFactory {
+	/// <summary>
+	/// 敵を生成する
+	/// </summary>
+	/// <param name="type">敵のタイプ</param>
+	/// <returns></returns>
 	public abstract GameObject Create(StgEnemyConstant.Type type);
 }
