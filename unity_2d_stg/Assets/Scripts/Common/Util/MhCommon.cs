df976267fbd7662452809a9c736e8426bc0bd476
Kur0qb <gdgdkuro@gmail.com>
2019-03-19 19:06:22 +0900

    suoファイルを管理対象外に修正 MhCommonの中括弧のフォーマットを統一、コメントを追加

--- unity_2d_stg/Assets/Scripts/Common/Util/MhCommon.cs
+++ unity_2d_stg/Assets/Scripts/Common/Util/MhCommon.cs
@@ -4,25 +4,32 @@ using UnityEngine;
 
 public class MhCommon {
 
-    // ログ出力
-    public static void Print(object message)
-    {
+    /// <summary>
+	/// デバッグ出力のラッパー
+	/// </summary>
+	/// <param name="message"></param>
+    public static void Print(object message) {
         Debug.Log(message);
     }
 
-    // アサーション
-    public static void Assert(int expr, object message)
-    {
-        if (expr == 0)
-        {
+    /// <summary>
+	/// アサーションのラッパー
+	/// </summary>
+	/// <param name="expr"></param>
+	/// <param name="message"></param>
+    public static void Assert(int expr, object message) {
+        if (expr == 0) {
             Debug.LogAssertion(message);
         }
     }
 
-    public static void Assert(bool expr, object message)
-    {
-        if (expr == false)
-        {
+	/// <summary>
+	/// アサーションのラッパー
+	/// </summary>
+	/// <param name="expr"></param>
+	/// <param name="message"></param>
+    public static void Assert(bool expr, object message) {
+        if (expr == false) {
             Debug.LogAssertion(message);
         }
     }
