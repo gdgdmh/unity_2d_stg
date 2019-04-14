cb71f54960de44d3147af6056d17fa0748a0d704
Kur0qb <gdgdkuro@gmail.com>
2019-04-15 00:24:30 +0900

    StgEnemyConstantDataにスコアの項目を実装

--- unity_2d_stg/Assets/Scripts/Stg/ShareData/ConstantData/StgEnemyConstantData.cs
+++ unity_2d_stg/Assets/Scripts/Stg/ShareData/ConstantData/StgEnemyConstantData.cs
@@ -5,8 +5,21 @@ using UnityEngine;
 public class StgEnemyConstantData {
 
 	public StgEnemyConstantData() {
+		constantDatas = null;
+	}
+
+	public void Initialize() {
 		// Enumの大きさから配列を作成
 		constantDatas = new float[System.Enum.GetNames(typeof(StgEnemyConstantDefine.Type)).Length];
+	}
+
+	/// <summary>
+	/// 仮の値を初期化
+	/// </summary>
+	public void InitializeValue() {
+		MhCommon.Assert(constantDatas != null, "StgEnemyConstantData::InitializeValue() constantDatas null please call Inintialize");
+		constantDatas[(int)StgEnemyConstantDefine.Type.kNormalEnemyScore] = 100.0f;
+        
 	}
 
 
