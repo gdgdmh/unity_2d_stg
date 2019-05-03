3023d023ed6368521603752e4eda3ce64bb8a79d
Kur0qb <gdgdkuro@gmail.com>
2019-05-03 23:49:16 +0900

    GameMainSceneTaskとGameScoreの実験用にコメントアウトしていた項目を削除

--- unity_2d_stg/Assets/Scripts/Scene/GameMainScene/GameMainSceneTask.cs
+++ unity_2d_stg/Assets/Scripts/Scene/GameMainScene/GameMainSceneTask.cs
@@ -12,28 +12,6 @@ public class GameMainSceneTask : MonoBehaviour
 
 	// Start is called before the first frame update
 	void Start() {
-
-		//enemyRandomPopper
-
-		/****
-		{
-			// 敵の生成
-			GameObject player = GameObject.Find("Player");
-			MhCommon.Assert(player != null, "GameMainSceneTask::Start() player null");
-			StgEnemyFactory factory = new StgEnemyFactory(player);
-			GameObject enemy = factory.Create(StgEnemyConstant.Type.kEnemyNormal);
-			Instantiate(enemy, new Vector3(0.0f, 4.0f, 0.0f), Quaternion.identity);
-		}
-		*/
-
-		/*
-		// PlayerBulletを動的生成
-		{
-			StgPlayerBulletFactory factory = new StgPlayerBulletFactory();
-			GameObject bullet = factory.Create(StgBulletConstant.Type.kPlayerNormal);
-			Instantiate(bullet, new Vector3(0.0f, -3.0f, 0.0f), Quaternion.identity);
-		}
-		*/
 		{
 			if (enemyRandomPopper == null) {
 				// プレイヤーのGameObjectを取得
@@ -46,51 +24,6 @@ public class GameMainSceneTask : MonoBehaviour
 				MhCommon.Print("random popper created");
 			}
 		}
-
-		//number002 = this.gameObject.AddComponent<DisplayNumber002>() as DisplayNumber002;
-		/*
-        // 動的にスクリプトを追加
-        if (stgPlayerController == null) {
-            stgPlayerController = this.gameObject.AddComponent<StgPlayerController>() as StgPlayerController;
-            MhCommon.Assert(stgPlayerController != null, "StgPlayer::Awake() StgPlayerController AddComponent failure");
-			// 試しにcall
-			stgPlayerController.Update();
-        }
-
-		 */
-		/*
-		frame1 = new DecrementCounterFrame();
-		frame1.SetCounter(10);
-		frame2 = new DecrementCounterElapsedTime();
-		frame2.SetCounter(10);
-		*/
-
-
-		/*
-		{
-			// DisplayNumber002 のスクリプトを取得
-			num002Object = GameObject.Find("DisplayNumber002");
-			MhCommon.Assert(num002Object != null, "GameMainSceneTask::DisplayNumberScript Find Failure");
-			displayNumber002Script = num002Object.GetComponent<DisplayNumber002>();
-			MhCommon.Assert(displayNumber002Script != null, "GameMainSceneTask::DisplayNumberScript Find Failure");
-			// 555に設定
-			displayNumber002Script.Set(555);
-			displayNumber002Script.SetBasePosition(new Vector3(2.5f, 4.5f, 0));
-			displayNumber002Script.SetOffset(new Vector3(-0.32f, 0, 0));
-			displayNumber002Script.ApplyPosition();
-		}
-		*/
-
-		/*
-		basePosition.x = 2.0f;
-		basePosition.y = 0;
-		basePosition.z = 0;
-		offset.x = -0.32f;
-		offset.y = 0;
-		offset.z = 0;
-
-		 * 
-		*/
     }
 
     // Update is called once per frame
@@ -99,15 +32,6 @@ public class GameMainSceneTask : MonoBehaviour
 		// 敵の湧きシステムを常に起動しておく
 		MhCommon.Assert(enemyRandomPopper != null, "GameMainSceneTask::Start() StgEnemyRandomPopper AddComponent failure");
 		enemyRandomPopper.TaskAppear();
-
-		/*
-		if (!frame2.IsTimeOver()) {
-			frame2.Update();
-			MhCommon.Print(frame2.GetCurrentCounter());
-		}
-		*/
-
-        
     }
 
 	private StgEnemyRandomPopper enemyRandomPopper;
@@ -115,6 +39,4 @@ public class GameMainSceneTask : MonoBehaviour
 	private DecrementCounterElapsedTime frame2;
 	private GameObject num002Object;
 	private DisplayNumber002 displayNumber002Script;
-
-	//private DisplayNumber002 number002;
 }
--- unity_2d_stg/Assets/Scripts/Stg/Game/Score/GameScore.cs
+++ unity_2d_stg/Assets/Scripts/Stg/Game/Score/GameScore.cs
@@ -8,19 +8,12 @@ using UnityEngine;
 public class GameScore : MonoBehaviour {
 
 	private void Awake() {
-		// Resourceから作成版
-		//displayNumber = (GameObject)Resources.Load("Prefabs/Common/Number/DisplayNumber002");
-		//MhCommon.Assert(displayNumber != null, "GameScore::Start() displayNumber null");
-		//displayNumber002 = displayNumber.GetComponent<DisplayNumber002>();
-		//MhCommon.Assert(displayNumber002 != null, "GameScore::Start() displayNumber002 null");
-		//Instantiate(displayNumber, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
 	}
 
 	// Start is called before the first frame update
 	void Start() {
 		{
 			// DisplayNumber002 のGameObjectとスクリプトを取得
-			//displayNumber = GameObject.Find("DisplayNumber002");
 			MhCommon.Assert(displayNumber != null, "GameScore::Awake DisplayNumber002 Find Failure");
 			displayNumber002 = displayNumber.GetComponent<DisplayNumber002>();
 			MhCommon.Assert(displayNumber002 != null, "GameScore::Awake DisplayNumber002(Component Script DisplayNumber002) Find Failure");
@@ -32,12 +25,6 @@ public class GameScore : MonoBehaviour {
 			//SetScale
 			displayNumber002.ApplyPosition();
 		}
-
-		// Resourceから作成版
-		//displayNumber002.Set(555);
-		//displayNumber002.SetBasePosition(new Vector3(2.5f, 4.5f, 0.0f));
-		//displayNumber002.SetOffset(new Vector3(-0.32f, 0.0f, 0.0f));
-		//displayNumber002.ApplyPosition();
     }
 
     // Update is called once per frame
