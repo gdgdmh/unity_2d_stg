2035ce62960440349efb4d6c0837b90ef814013f
Kur0qb <gdgdkuro@gmail.com>
2019-05-18 13:10:00 +0900

    StgPlayerHealthにplayer情報を持たせるように修正

--- unity_2d_stg/Assets/Scripts/Stg/Game/Player/StgPlayerHealth.cs
+++ unity_2d_stg/Assets/Scripts/Stg/Game/Player/StgPlayerHealth.cs
@@ -2,21 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StgPlayerHealth : MonoBehaviour
-{
+public class StgPlayerHealth : MonoBehaviour {
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         // HPゲージ取得
        CanvasImageGauge gauge = GameObject.Find("Gauge").GetComponent<CanvasImageGauge>();
         // 0.2にテストで設定
-        gauge.SetGaugeRaw(0.2f);
-        
+        //gauge.SetGaugeRaw(0.2f);
+        gauge.SetGaugeRaw(1.0f);
+
+        MhCommon.Assert(player != null, "StgPlayerHealth::Start() player null");
+        playerScript = this.player.GetComponent<StgPlayer>();
+        MhCommon.Assert(playerScript != null, "StgPlayerHealth::Start() playerScript null");
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {        
     }
+
+    //public void SetPlayer(ref GameObject player) {
+    //}
+
+    //public void SetStgPlayer(ref GameObject player) {
+    //}
+
+	public GameObject player;
+    private StgPlayer playerScript;
 }
