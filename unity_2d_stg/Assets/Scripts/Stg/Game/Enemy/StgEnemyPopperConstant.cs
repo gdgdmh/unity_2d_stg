b3db33addd07c41ea5c8d9144ef32a06cba7bcdc
Kur0qb <gdgdkuro@gmail.com>
2019-06-15 22:24:27 +0900

    StgEnemyPopperConstantとStgEnemyPopperDataを追加

---
+++ unity_2d_stg/Assets/Scripts/Stg/Game/Enemy/StgEnemyPopperConstant.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StgEnemyPopperConstant {
+	// 敵がPopするタイプ
+    public enum PopType : int {
+        kTime, // 時間による
+        kNobody, // 敵がいないとき
+        kBoss,  // ボス用
+    };
+
+}
---
+++ unity_2d_stg/Assets/Scripts/Stg/Game/Enemy/StgEnemyPopperData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StgEnemyPopperData {
+
+    public StgEnemyPopperData() {
+        popType = StgEnemyPopperConstant.PopType.kTime;
+        popTime = 0;
+    }
+
+    public StgEnemyPopperData(StgEnemyPopperConstant.PopType popType, int popTime) {
+        this.popType = popType;
+        this.popTime = popTime;
+    }
+
+    private StgEnemyPopperConstant.PopType popType; // 出現タイプ
+    private int popTime; // 出現時間(出現タイプが時間のときに使用)
+}
