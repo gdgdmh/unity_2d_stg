aa425389d5854ddc7c0d3044ee0560c48a16904c
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2019-07-29 01:10:46 +0900

    Unityでリソースが見つからなかった場合の例外クラスを追加

---
+++ unity_2d_stg/Assets/Scripts/Common/System/Exception/UnityResourceNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// リソースロード時に見つからなかった場合の例外
+/// </summary>
+public class UnityResourceNotFoundException : System.Exception {
+    UnityResourceNotFoundException() {
+    }
+}
