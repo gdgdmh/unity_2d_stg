adbdff15fdd54cdd9bab8c61c0c682e7e17cb7cc
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2019-12-08 17:16:58 +0900

    他のjsonパーサーのコードを削除(コード整理)

--- unity_2d_stg/Assets/Scripts/Stg/Game/Enemy/StgEnemyLoadResourceStageJson.cs
+++ unity_2d_stg/Assets/Scripts/Stg/Game/Enemy/StgEnemyLoadResourceStageJson.cs
@@ -13,86 +13,21 @@ public class StgEnemyLoadResourceStageJson {
         StgStageJsonEnemyLaunchDatas jsonDatas = new StgStageJsonEnemyLaunchDatas();
         //T jsonItem = default(T);
         if (result) {
-            //var data = JsonUtility.FromJson<StgStageJsonEnemyLaunchDataArray>(textAsset.Get());
+            // テキストデータ取得
             var data = textAsset.Get();
-            //LoadJson.Load
-
-            //Utf8Json utf8Json = new Utf8Json();
-
-            /*
- public class NewBehaviourScript : MonoBehaviour {
-	void Start () {
-        var json2 = JsonSerializer.Deserialize<dynamic>(@"{""foo"":""json"",""bar"":100,""nest"":{""foobar"":true}}");
-
-        var r1 = json2["foo"]; // "json" - dynamic(string)
-        var r2 = json2["bar"]; // 100 - dynamic(double), it can cast to int or other number.
-        var r3 = json2["nest"]["foobar"]; // true
-    }
-}
-            */
+            // jsonパース
             var json2 = Utf8Json.JsonSerializer.Deserialize<dynamic>(data);
-            //RuntimeBinderException: Cannot implicitly convert type 'System.Collections.Generic.Dictionary<string,object>
             System.Collections.Generic.Dictionary<string, object> json3 = json2["stage_data"];
 
-            foreach (KeyValuePair<string, object> item in json3) {
-                //var key = item.Key;
-                //var val = item.Value;
-
-                Dictionary<string, object> stageParameters = (Dictionary<string, object>)item.Value;
+            foreach (KeyValuePair<string, object> stageDatas in json3) {
+                Dictionary<string, object> stageParameters = (Dictionary<string, object>)stageDatas.Value;
                 foreach (KeyValuePair<string, object> stageData in stageParameters) {
                     var key = stageData.Key;
                     var val = stageData.Value;
 
                     int bbb = 10;
-
                 }
-
-                //KeyValuePair<string, object> stageParameters = (System.Collections.Generic.KeyValuePair<string, object>)item.Value;
-
-                //foreach (KeyValuePair<string,object> stageParameter in stageParameters) {
-                //}
-                //var ad = item.Value;
-                //int bbb = 10;
-
             }
-
-            //for (int i = 0; i < ; ++i) {
-            //}
-
-
-            /*
-            SimpleJSON.JSONNode node = SimpleJSON.JSONNode.Parse(data);
-
-            var title = node["stage_data"]["0"]["enemy_type"];
-            var test_data = node["stage_data"]["10000"]["enemy_type"];
-            //foreach (SimpleJSON.JSONNode node["stage_data"] in aaa) {
-            //}
-            StgStageJsonEnemyLaunchData[] launchDatas = new StgStageJsonEnemyLaunchData[node.Count];
-
-            for (int i = 0; i < node.Count; ++i) {
-                StgStageJsonEnemyLaunchData launchData = new StgStageJsonEnemyLaunchData();
-                launchData.enemy_type = node["stage_data"][i]["enemy_type"];
-                launchData.frame = node["stage_data"][i]["frame"];
-                launchData.x = node["stage_data"][i]["x"];
-                launchData.y = node["stage_data"][i]["y"];
-                launchData.z = node["stage_data"][i]["z"];
-                launchDatas[i] = launchData;
-            }
-            */
-
-            var aaa = 10;
-
-
-
-
-            /*
-            StgStageJsonEnemyLaunchData launchData = new StgStageJsonEnemyLaunchData();
-            launchData.enemy_type = node["stage_data"];
-            launchData.frame = ;
-            launchData.x = ;
-            launchData.y = ;
-            launchData.z = ;
-            */
 
             /*
     public string enemy_type;
@@ -101,11 +36,7 @@ public class StgEnemyLoadResourceStageJson {
     public float y;
     public float z;         
             */
-
-            //data = null;
         }
-        //return jsonItem;
-
         //StgStageJsonEnemyLaunchDatas jsonDatas = LoadJson.LoadTest<StgStageJsonEnemyLaunchDatas, StgStageJsonEnemyLaunchData>(resourcePath);
         //StgStageJsonEnemyLaunchData[] jsonDatas = LoadJson.LoadTest<StgStageJsonEnemyLaunchDatas, StgStageJsonEnemyLaunchData>(resourcePath);
 
