4ad43e6245d52affb7c0ecb906832d459bf33238
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2019-12-10 22:37:53 +0900

    弾生成の注釈が間違っていたのを修正

--- unity_2d_stg/Assets/Scripts/Stg/Game/Bullet/AStgBulletFactory.cs
+++ unity_2d_stg/Assets/Scripts/Stg/Game/Bullet/AStgBulletFactory.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public abstract class AStgBulletFactory {
 
-	/// <summary>
-	/// 弾の生成
-	/// </summary>
-	/// <param name="type">弾の種類</param>
-	/// <param name="position">弾の位置</param>
-	/// <returns>成功したら弾ベースクラス失敗したらnull</returns>
+    /// <summary>
+    /// 弾の生成
+    /// </summary>
+    /// <param name="type">弾の種類</param>
+    /// <returns>生成された弾オブジェクト</returns>
 	public abstract GameObject Create(StgBulletConstant.Type type);
 }
