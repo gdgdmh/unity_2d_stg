13a352a14ae9fdc3b8fd21ff4f7ef86600e00bad
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-01-01 22:18:35 +0900

    敵出現のアルゴリズムをログレベルで実装

--- unity_2d_stg/Assets/Scripts/Stg/Game/Enemy/StgEnemyJsonDataPopper.cs
+++ unity_2d_stg/Assets/Scripts/Stg/Game/Enemy/StgEnemyJsonDataPopper.cs
@@ -21,6 +21,9 @@ public class StgEnemyJsonDataPopper : MonoBehaviour, IStgEnemyAppearable {
 
         StgStageJsonEnemyLaunchData d = stgStageJsonEnemyLaunchDatas.Get(4);
         d.Print();
+
+		currentIndex = 0;
+		maxCount = stgStageJsonEnemyLaunchDatas.GetCount();
     }
 
     // Start is called before the first frame update
@@ -37,7 +40,31 @@ public class StgEnemyJsonDataPopper : MonoBehaviour, IStgEnemyAppearable {
 
 		// 総合経過時間を加算
 		totalTime += elapsedTime;
-		//Debug.Log(string.Format("time={0}", totalTime));
+		Debug.Log(string.Format("time={0}", totalTime));
+
+		// データがない or データが終端まで実行し終わっている
+		if ((maxCount == 0) || (currentIndex >= maxCount)) {
+			Debug.Log("ended");
+			return;
+		}
+
+		bool isContinue = true;
+		while (isContinue) {
+			if (currentIndex >= maxCount) {
+				// データが終端まで実行している
+				Debug.Log("loop end");
+				return;
+			}
+			StgStageJsonEnemyLaunchData launchData = stgStageJsonEnemyLaunchDatas.Get(currentIndex);
+			if (launchData.time <= (int)totalTime) {
+				launchData.Print();
+				++currentIndex;
+				Debug.Log("next loop");
+			} else {
+				Debug.Log("loop end by frame");
+				return;
+			}
+		}
 
 		/*
 		if (!counter.IsTimeOver()) {
@@ -61,9 +88,11 @@ public class StgEnemyJsonDataPopper : MonoBehaviour, IStgEnemyAppearable {
         */
 	}
 
-	private GameObject player;
-	private StgEnemyFactory enemyFactory;
-    private StgStageJsonEnemyLaunchDatas stgStageJsonEnemyLaunchDatas = new StgStageJsonEnemyLaunchDatas();
-	private float totalTime;
+	private GameObject player;				// プレイヤー
+	private StgEnemyFactory enemyFactory;	// 敵生成システム
+    private StgStageJsonEnemyLaunchDatas stgStageJsonEnemyLaunchDatas = new StgStageJsonEnemyLaunchDatas(); // 敵出現データ
+	private float totalTime;	// 現在の経過時間
+	private int currentIndex;   // 敵出現データのindex
+	private int maxCount;		// 敵出現データの最大数
 
 }
