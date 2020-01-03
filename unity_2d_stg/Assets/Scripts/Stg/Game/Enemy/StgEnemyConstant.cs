0ac86ca4b1547a3d17d554a511d2ae5ee58b2893
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-01-03 15:19:26 +0900

    敵文字列の定義を追加、コメントが間違っていたのを修正

--- unity_2d_stg/Assets/Scripts/Stg/Game/Enemy/StgEnemyConstant.cs
+++ unity_2d_stg/Assets/Scripts/Stg/Game/Enemy/StgEnemyConstant.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class StgEnemyConstant {
-	// プレイヤーの弾の種類
-    public enum Type : int {
+	// 敵の種類
+    public enum Type {
 		kEnemyNormal,
         kStraightMoveEnemy,
 		kUnknown
     };
+
+	public static readonly string kStringEnemyNormal = "normal_enemy";
+	public static readonly string kStringStraightMoveEnemy = "straight_move_enemy";
+	public static readonly string kUnknown = "";
+
+	public static Type GetStringToType(string enemyType) {
+		if (enemyType == kStringEnemyNormal) {
+			return Type.kEnemyNormal;
+		} else if (enemyType == kStringStraightMoveEnemy) {
+			return Type.kStraightMoveEnemy;
+		}
+		return Type.kUnknown;
+	}
 }
