40828075c607708158862f07413a38c977a0d4e2
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-01-29 16:41:08 +0900

    発射間隔の更新が間違っていたのを修正、コメントを追加

--- unity_2d_stg/Assets/Scripts/Stg/Game/Player/StgPlayerIntervalShot.cs
+++ unity_2d_stg/Assets/Scripts/Stg/Game/Player/StgPlayerIntervalShot.cs
@@ -2,20 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// プレイヤーの弾を発射する(インターバル設定)
+/// 発射位置はshotPositionとoffsetの足しあわせによって確定
+/// </summary>
 public class StgPlayerIntervalShot {
 	public StgPlayerIntervalShot() {
 	}
 
+	/// <summary>
+	/// 初期化
+	/// </summary>
 	public void Initialize() {
 		shotInterval = 1.0f;
+		currentShotInterval = shotInterval;
 		shotPosition = new Vector3(0.0f, 0.0f, 0.0f);
 		offset = new Vector3(0.0f, 0.0f, 0.0f);
 	}
 
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	/// <param name="elapsedTime">前回からの経過時間</param>
+	/// <param name="touchAction">タッチ情報</param>
+	/// <param name="dragAction">ドラッグ情報</param>
 	public void Update(float elapsedTime, UnitySingleTouchAction touchAction, UnitySingleTouchDragAction dragAction) {
 		// 一定時間ごとに攻撃
-		shotInterval -= elapsedTime;
-		if (shotInterval <= 0.0f) {
+		currentShotInterval -= elapsedTime;
+		if (currentShotInterval <= 0.0f) {
 			// 攻撃処理
 			AttackProcess();
 			// 攻撃再間隔設定
@@ -23,19 +37,33 @@ public class StgPlayerIntervalShot {
 		}
 	}
 
+	/// <summary>
+	/// 発射間隔のリセット
+	/// </summary>
 	public void ResetInterval() {
 		currentShotInterval = shotInterval;
 	}
 
+	/// <summary>
+	/// 発射間隔の設定
+	/// </summary>
+	/// <param name="interval">発射間隔</param>
 	public void SetInterval(float interval) {
 		shotInterval = interval;
 	}
 
-
+	/// <summary>
+	/// 発射位置の設定
+	/// </summary>
+	/// <param name="position">発射位置</param>
 	public void SetShotPosition(Vector3 position) {
 		shotPosition = position;
 	}
 
+	/// <summary>
+	/// 発射位置オフセットの設定
+	/// </summary>
+	/// <param name="offset">発射位置のオフセット</param>
 	public void SetShotOffset(Vector3 offset) {
 		this.offset = offset;
 	}
