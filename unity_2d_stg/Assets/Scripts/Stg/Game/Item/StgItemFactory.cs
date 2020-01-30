8ec41ee4dd0366736fc3d009574657e61c024513
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-01-30 23:03:31 +0900

    アイテムのファクトリークラスを仮追加

---
+++ unity_2d_stg/Assets/Scripts/Stg/Game/Item/StgItemFactory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StgItemFactory {
+	public StgItemFactory() {
+	}
+
+	public GameObject Create(StgItemConstant.Type type) {
+		if (type == StgItemConstant.Type.kPowerup) {
+		} else if (type == StgItemConstant.Type.kScoreup) {
+		}
+		return null;
+	}
+}
