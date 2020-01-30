683489b4a9551ce144925bc808f468c6e06001a5
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-01-30 22:55:03 +0900

    アイテム用の定数クラスを追加

---
+++ unity_2d_stg/Assets/Scripts/Stg/Game/Item/StgItemConstant.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StgItemConstant {
+	public enum Type {
+		kPowerup,
+		kScoreup,
+		kUnknown
+	};
+}
