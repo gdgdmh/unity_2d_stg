485e4f32fd010c416a9f7c522a4c8f844edaaadc
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-02-03 18:38:26 +0900

    アイテムドロップインターフェースを追加

---
+++ unity_2d_stg/Assets/Scripts/Stg/Game/Item/IStgItemDroppable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// アイテムドロップするインターフェース
+/// </summary>
+interface IStgItemDroppable {
+	/// <summary>
+	/// アイテムをドロップする
+	/// </summary>
+	void Drop();
+}
